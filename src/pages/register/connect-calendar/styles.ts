7dbc3114120254cada43e08ffd8e20ae050dd64c
Eduardo Tavares <eduardotavares@Eduardos-MacBook-Air.local>
2023-07-24 12:52:00 -0300

    criando/estilizando a pagina connect-calendar

---
+++ src/pages/register/connect-calendar/styles.ts
@@ -0,0 +1,17 @@
+import { Box, styled } from '@ignite-ui/react'
+
+export const ConnectBox = styled(Box, {
+  display: 'flex',
+  flexDirection: 'column',
+  marginTop: '$6',
+})
+
+export const ConnectItem = styled('div', {
+  alignItems: 'center',
+  border: '1px solid $gray600',
+  borderRadius: '#md',
+  display: 'flex',
+  justifyContent: 'space-between',
+  marginBottom: '$2',
+  padding: '$16 $24',
+})
