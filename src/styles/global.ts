bc14b31c672ffb7b0f69acccdb9139e916b69a8d
Eduardo Tavares <eduardotavares@Eduardos-MacBook-Air.local>
2023-08-11 22:12:47 -0300

    problemas de prettier/lint no deploy

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,7 +4,7 @@ export const globalStyles = globalCss({
   '*': {
     boxSizing: 'border-box',
     margin: 0,
-    padding: 0
+    padding: 0,
   },
 
   body: {
@@ -13,5 +13,5 @@ export const globalStyles = globalCss({
 
     backgroundColor: '$gray900',
     color: '$gray100',
-  }
+  },
 })
